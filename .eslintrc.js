432485d58a4645e42e568843fdcdb4009c760706
Uriel Van <urielvan@gmail.com>
2020-09-17 11:24:16 +0800

    chore: basic files

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    es2020: true,
+    node: true,
+  },
+  ignorePatterns: [
+    '.eslintrc.js',
+  ],
+  extends: [
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  plugins: [
+    '@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'arrow-parens': ['error', 'as-needed'],
+    'max-classes-per-file': 'off',
+    'no-console': 'off',
+    'no-param-reassign': 'off',
+    'no-shadow': 'off',
+    'no-use-before-define': 'off',
+    'no-void': 'off',
+    '@typescript-eslint/no-use-before-define': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        ignoreRestSiblings: true,
+        argsIgnorePattern: '^_',
+      },
+    ],
+  },
+};
